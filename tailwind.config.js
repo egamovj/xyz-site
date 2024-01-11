56d34ad023bb5c48d81afd0cffa621d1ac4b3acf
Jo'rabek Egamov <134671078+egamovj@users.noreply.github.com>
2024-01-11 21:20:28 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#010851",
+        'secondary': "#9A7AF1",
+        "tartiary": "#707070",
+        "pink": "#EE9AE5"
+      }
+    },
+  },
+  plugins: [],
+}
